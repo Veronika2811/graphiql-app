a8a5ae624d9d8e9679824b36058b5e760d5dc53c
Arterixs <artemontugg@gmail.com>
2023-12-13 13:15:47 +0300

    feat: add routing and alias #5

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,5 @@
+import { Box } from '@mui/material';
+
+const NotFound = () => <Box component="div">Not Found Page</Box>;
+
+export default NotFound;
---
+++ src/pages/welcome/index.tsx
@@ -0,0 +1,5 @@
+import { Box } from '@mui/material';
+
+const WelcomePage = () => <Box component="div">Welcome Page</Box>;
+
+export default WelcomePage;
