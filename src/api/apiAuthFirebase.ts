f2b2572198e267bb5a01bf639bfd74e404c26f63
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-12-18 13:54:26 +0200

    refactor: change api

--- src/api/apiAuthFirebase.ts
+++ src/api/apiAuthFirebase.ts
@@ -1,71 +1,88 @@
-import { FirebaseError } from 'firebase/app';
+import { FirebaseApp, FirebaseError } from 'firebase/app';
 import {
+  Auth,
   createUserWithEmailAndPassword,
   fetchSignInMethodsForEmail,
   getAuth,
   signInWithEmailAndPassword,
   signOut,
 } from 'firebase/auth';
-import { addDoc, collection, getFirestore } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  Firestore,
+  getFirestore,
+} from 'firebase/firestore';
 
 import { ERROR } from '../constants/constants';
 import { User } from '../type/interface';
 
 import { AUTH_PROVIDERS, COLLECTIONS } from './constants/constants';
-import { app } from './initFirebase';
+// import { app } from './initFirebase';
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
 
-export const signIn = async ({ email, password }: User): Promise<void> => {
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-  } catch (error) {
-    if (error instanceof FirebaseError) {
-      throw new Error(
-        `${ERROR.FIREBASE.HEADER}: ${error.code} - ${error.message}`
+export default class AuthService {
+  private auth: Auth;
+
+  private db: Firestore;
+
+  constructor(app: FirebaseApp) {
+    this.auth = getAuth(app);
+    this.db = getFirestore(app);
+  }
+
+  signIn = async ({ email, password }: User): Promise<void> => {
+    try {
+      await signInWithEmailAndPassword(this.auth, email, password);
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        throw new Error(
+          `${ERROR.FIREBASE.HEADER}: ${error.code} - ${error.message}`
+        );
+      }
+      throw new Error(ERROR.FIREBASE.MESSAGE);
+    }
+  };
+
+  registerUser = async ({ name, email, password }: User): Promise<void> => {
+    try {
+      const res = await createUserWithEmailAndPassword(
+        this.auth,
+        email,
+        password
       );
+      const { user } = res;
+      await addDoc(collection(this.db, COLLECTIONS.USERS), {
+        uid: user.uid,
+        name,
+        authProvider: AUTH_PROVIDERS.LOCAL,
+        email,
+      });
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        throw new Error(`${ERROR.FIREBASE}: ${error.code} - ${error.message}`);
+      }
+      throw new Error(ERROR.FIREBASE.MESSAGE);
     }
-    throw new Error(ERROR.FIREBASE.MESSAGE);
-  }
-};
+  };
 
-export const registerUser = async ({
-  name,
-  email,
-  password,
-}: User): Promise<void> => {
-  try {
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-    const { user } = res;
-    await addDoc(collection(db, COLLECTIONS.USERS), {
-      uid: user.uid,
-      name,
-      authProvider: AUTH_PROVIDERS.LOCAL,
-      email,
-    });
-  } catch (error) {
-    if (error instanceof FirebaseError) {
-      throw new Error(`${ERROR.FIREBASE}: ${error.code} - ${error.message}`);
+  checkEmail = async (email: string): Promise<boolean | void> => {
+    try {
+      const signInMethods = await fetchSignInMethodsForEmail(this.auth, email);
+      return !!signInMethods.length;
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        throw new Error(
+          `${ERROR.FIREBASE.HEADER}: ${error.code} - ${error.message}`
+        );
+      }
+      throw new Error(ERROR.FIREBASE.MESSAGE);
     }
-    throw new Error(ERROR.FIREBASE.MESSAGE);
-  }
-};
+  };
 
-export const checkEmail = async (email: string): Promise<boolean | void> => {
-  try {
-    const signInMethods = await fetchSignInMethodsForEmail(auth, email);
-    return !!signInMethods.length;
-  } catch (error) {
-    if (error instanceof FirebaseError) {
-      throw new Error(
-        `${ERROR.FIREBASE.HEADER}: ${error.code} - ${error.message}`
-      );
-    }
-    throw new Error(ERROR.FIREBASE.MESSAGE);
-  }
-};
-
-export const logout = () => {
-  signOut(auth);
-};
+  logout = () => {
+    signOut(this.auth);
+  };
+}
--- src/api/apiStoreFirebase.ts
+++ src/api/apiStoreFirebase.ts
@@ -1,7 +1,7 @@
-import { FirebaseError } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { FirebaseApp, FirebaseError } from 'firebase/app';
 import {
   collection,
+  Firestore,
   getDocs,
   getFirestore,
   query,
@@ -11,35 +11,43 @@ import {
 import { ERROR } from '../constants/constants';
 
 import { COLLECTIONS, USER_FIELDS } from './constants/constants';
-import { app } from './initFirebase';
+// import { app } from './initFirebase';
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
 
-export const getUserName = async (uid: string): Promise<string | void> => {
-  try {
-    const userQuery = query(
-      collection(db, COLLECTIONS.USERS),
-      where(USER_FIELDS.UID, '==', uid)
-    );
-    const userQuerySnap = await getDocs(userQuery);
+export default class DataBaseService {
+  private db: Firestore;
 
-    if (!userQuerySnap.docs.length) {
-      throw new Error(ERROR.FIREBASE.NOT_FOUND);
-    }
-
-    const userName = userQuerySnap.docs[0].data().name;
-
-    return userName;
-  } catch (error) {
-    if (error instanceof FirebaseError) {
-      throw new Error(
-        `${ERROR.FIREBASE.HEADER}: ${error.code} - ${error.message}`
-      );
-    }
-    if (error instanceof Error) {
-      throw new Error(error.message);
-    }
-    throw new Error(ERROR.FIREBASE.MESSAGE);
+  constructor(app: FirebaseApp) {
+    this.db = getFirestore(app);
   }
-};
+
+  getUserName = async (uid: string): Promise<string | void> => {
+    try {
+      const userQuery = query(
+        collection(this.db, COLLECTIONS.USERS),
+        where(USER_FIELDS.UID, '==', uid)
+      );
+      const userQuerySnap = await getDocs(userQuery);
+
+      if (!userQuerySnap.docs.length) {
+        throw new Error(ERROR.FIREBASE.NOT_FOUND);
+      }
+
+      const userName = userQuerySnap.docs[0].data().name;
+
+      return userName;
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        throw new Error(
+          `${ERROR.FIREBASE.HEADER}: ${error.code} - ${error.message}`
+        );
+      }
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+      throw new Error(ERROR.FIREBASE.MESSAGE);
+    }
+  };
+}
