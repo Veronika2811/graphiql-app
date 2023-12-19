42e4eb05b1a903e847c8cbc496b44eca7259bb44
Veranika <smeyun@list.ru>
2023-12-19 11:28:40 +0300

    feat: add section About Team

---
+++ src/components/TeamCard/TeamCard.styles.ts
@@ -0,0 +1,22 @@
+export const styles = {
+  card: {
+    display: 'flex',
+    borderRadius: '20px',
+    justifyContent: 'space-between',
+    padding: '20px',
+    maxWidth: '500px',
+  },
+  'card-content': {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px',
+  },
+  'card-social': {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+  },
+  'card-image': {
+    width: '130px',
+  },
+};
---
+++ src/components/TeamCard/index.tsx
@@ -0,0 +1,55 @@
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Typography,
+} from '@mui/material';
+
+import girl from '../../assets/images/svg/girl.svg';
+import man from '../../assets/images/svg/man.svg';
+import { GithubIcon } from '../../ui/icons/GitHubIcon';
+import { LinkedInIcon } from '../../ui/icons/LinkedInIcon';
+import { TelegramIcon } from '../../ui/icons/TelegramIcon';
+
+import { styles } from './TeamCard.styles';
+
+interface TeamCardProps {
+  name: string;
+  github: string;
+  linkedIn: string;
+  telegram: string;
+}
+
+export const TeamCard = ({
+  name,
+  github,
+  linkedIn,
+  telegram,
+}: TeamCardProps) => (
+  <Card sx={styles.card}>
+    <CardContent sx={styles['card-content']}>
+      <Typography variant="subtitle1" component="h5">
+        {name}
+      </Typography>
+      <Box sx={styles['card-social']}>
+        <IconButton href={github} target="_blank">
+          <GithubIcon size="large" />
+        </IconButton>
+        <IconButton href={linkedIn} target="_blank">
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton href={telegram} target="_blank">
+          <TelegramIcon />
+        </IconButton>
+      </Box>
+    </CardContent>
+    <CardMedia
+      component="img"
+      image={name === 'Veranika' ? girl : man}
+      alt={name}
+      sx={styles['card-image']}
+    />
+  </Card>
+);
