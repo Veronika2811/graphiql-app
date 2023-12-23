cbd0cfe43e99fab8bf4a37673143f4ca1e28f9a6
Veranika <smeyun@list.ru>
2023-12-24 01:13:39 +0300

    fix: delete Provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from 'layouts/default';
 import { SuspenseLayout } from 'layouts/suspense';
 import { EDITOR, WELCOME } from 'shared/router-path';
-import { store } from 'store';
 
 const WelcomePage = React.lazy(() => import('pages/welcome'));
 const EditorPage = React.lazy(() => import('pages/editor'));
@@ -17,14 +15,7 @@ export const App = () => (
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate to={WELCOME} />} />
         <Route path={WELCOME} element={<WelcomePage />} />
-        <Route
-          path={EDITOR}
-          element={
-            <Provider store={store}>
-              <EditorPage />
-            </Provider>
-          }
-        />
+        <Route path={EDITOR} element={<EditorPage />} />
       </Route>
     </Route>
   </Routes>
