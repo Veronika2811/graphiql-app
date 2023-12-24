1434e124dea32a84b8bb67faed1f276037f26af3
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-12-24 10:48:05 +0200

    fix: change styles

--- src/components/CustomTab/index.tsx
+++ src/components/CustomTab/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Tab, Tabs } from '@mui/material';
 
+import generateStyles from './style';
+
 interface TabPanelProps {
   children: React.ReactNode;
   index: number;
@@ -20,6 +22,8 @@ const TabPanel = ({ children, value, index }: TabPanelProps) => {
 
 const CustomTab = ({ tabNames, tabContent, activeTab = 0 }: CustomTabProps) => {
   const [value, setValue] = useState(activeTab);
+  const styles = generateStyles(tabNames.length);
+
   const handleChange = (_: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
@@ -31,14 +35,10 @@ const CustomTab = ({ tabNames, tabContent, activeTab = 0 }: CustomTabProps) => {
         onChange={handleChange}
         centered
         indicatorColor="secondary"
-        sx={{ mb: 4 }}
+        sx={styles.tabs}
       >
-        {tabNames.map((item, _, array) => (
-          <Tab
-            key={item}
-            label={item}
-            sx={{ width: `${100 / array.length}%` }}
-          />
+        {tabNames.map((item) => (
+          <Tab key={item} label={item} sx={styles.tab} />
         ))}
       </Tabs>
       {tabContent.map((item, index) => (
