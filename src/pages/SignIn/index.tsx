5c01d4a183c1a2c70fd54ac612730c355deeb937
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-02 15:21:29 +0200

    feat: add FrameBox component

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,22 +1,17 @@
-import { Link, Typography } from '@mui/material';
 import { ROOT } from 'shared/constants/elements';
+import { SIGN_UP } from 'shared/router-path';
 
-import { ContainerForm } from 'components/ContainerForm';
+import { FrameBox } from 'components/FrameBox';
 import { SignInForm } from 'components/SignInForm';
 
 const SignIn = () => (
-  <ContainerForm title={ROOT.SIGN_IN.TITLE}>
-    <SignInForm />
-    <Typography
-      variant="body1"
-      component="p"
-      align="center"
-      sx={{ fontSize: '1rem' }}
-    >
-      {`${ROOT.SIGN_IN.MESSAGE} `}
-      <Link href="/">{ROOT.SIGN_IN.LINK_TEXT}</Link>
-    </Typography>
-  </ContainerForm>
+  <FrameBox
+    title={ROOT.SIGN_IN.TITLE}
+    content={<SignInForm />}
+    message={ROOT.SIGN_IN.MESSAGE}
+    linkAdress={SIGN_UP}
+    linkText={ROOT.SIGN_IN.LINK_TEXT}
+  />
 );
 
 export default SignIn;
