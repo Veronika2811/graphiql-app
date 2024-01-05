c93e8ed17d4c28537b6311cab65fff12c7efba35
Konstantin Kikinov <kikinov.k@gmail.com>
2024-01-05 14:01:05 +0200

    feat: add Private router #16

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,5 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
 import {
   AppBar,
   Box,
@@ -7,37 +9,63 @@ import {
   Switch,
   Typography,
 } from '@mui/material';
+import { auth } from 'api/initFirebase';
+import { SIGN_IN, SIGN_UP } from 'shared/router-path';
 import { theme } from 'theme';
 import { GraphQLIcon } from 'ui/icons';
 
+import { Logout } from 'components/Logout';
+
 import { headerSx } from './styles';
 
-export const Header = () => (
-  <AppBar position="sticky" sx={headerSx.header}>
-    <Container maxWidth="xl" sx={headerSx.container}>
-      <GraphQLIcon width={theme.spacing(7)} height={theme.spacing(7)} />
-      <Stack direction="row" spacing={2.5}>
-        <Box component="div" display="flex" alignItems="center" gap={1}>
-          <Typography variant="button" color="primary.main">
-            Ru
-          </Typography>
-          <Switch
-            sx={headerSx.switch}
-            inputProps={{ 'aria-label': 'controlled', role: 'switch' }}
-          />
-          <Typography variant="button" color="primary.main">
-            En
-          </Typography>
-        </Box>
-        <Stack direction="row" spacing={2.5} alignItems="center">
-          <Button variant="outlined" size="large" sx={headerSx.button}>
-            Sign In
-          </Button>
-          <Button variant="outlined" size="large" sx={headerSx.button}>
-            Sign Up
-          </Button>
+export const Header = () => {
+  const [user] = useAuthState(auth);
+
+  return (
+    <AppBar position="sticky" sx={headerSx.header}>
+      <Container maxWidth="xl" sx={headerSx.container}>
+        <GraphQLIcon width={theme.spacing(7)} height={theme.spacing(7)} />
+        <Stack direction="row" spacing={2.5}>
+          <Box component="div" display="flex" alignItems="center" gap={1}>
+            <Typography variant="button" color="primary.main">
+              Ru
+            </Typography>
+            <Switch
+              sx={headerSx.switch}
+              inputProps={{ 'aria-label': 'controlled', role: 'switch' }}
+            />
+            <Typography variant="button" color="primary.main">
+              En
+            </Typography>
+          </Box>
+          <Stack direction="row" spacing={2.5} alignItems="center">
+            {user ? (
+              <Logout sx={headerSx.button} />
+            ) : (
+              <>
+                <Button
+                  component={Link}
+                  to={SIGN_IN}
+                  variant="outlined"
+                  size="large"
+                  sx={headerSx.button}
+                >
+                  Sign In
+                </Button>
+                <Button
+                  component={Link}
+                  to={SIGN_UP}
+                  variant="outlined"
+                  size="large"
+                  sx={headerSx.button}
+                >
+                  Sign Up
+                </Button>
+              </>
+            )}
+          </Stack>
         </Stack>
-      </Stack>
-    </Container>
-  </AppBar>
-);
+      </Container>
+    </AppBar>
+  );
+};
