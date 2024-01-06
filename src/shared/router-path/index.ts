7bbf903283a871dce3ad68ec126222a4d8153bcc
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-06 02:07:04 +0200

    fix: change router-path

--- src/shared/router-path/index.ts
+++ src/shared/router-path/index.ts
@@ -3,4 +3,4 @@ export const EDITOR = '/editor';
 export const AUTH = '/auth';
 export const SIGN_IN = '/sign-in';
 export const SIGN_UP = '/sign-up';
-export const AUTHORIZATION = '/authorization';
+export const AUTHORIZATION = '/auth';
